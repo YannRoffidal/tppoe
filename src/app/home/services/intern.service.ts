4686ebcad60eee9272dc5358cd02f4d656809a0f
SAJ <stoneroff@gmail.com>
2024-03-25 15:37:49 +0100

    full intern list and modified title

--- src/app/home/services/intern.service.ts
+++ src/app/home/services/intern.service.ts
@@ -7,12 +7,68 @@ import { Intern } from '../types/intern.type';
 export class InternService {
   private _interns: Array<Intern> = [
     {
-      lastname: 'Sparrow',
-      firstname: 'Jack'
+      lastname: 'ARNAUD',
+      firstname: 'Manon'
     },
     {
-      lastname: 'Casper',
-      firstname: 'Manon'
+      lastname: 'AUBERT',
+      firstname: 'Jean-luc'
+    },
+    {
+      lastname: 'BERNARDIN',
+      firstname: 'Bruno'
+    },
+    {
+      lastname: 'BERTON',
+      firstname: 'Dorine'
+    },
+    {
+      lastname: 'BETTAN',
+      firstname: 'Frédéric'
+    },
+    {
+      lastname: 'BONNET',
+      firstname: 'Fabien'
+    },
+    {
+      lastname: 'CHENU',
+      firstname: 'Guillaume'
+    },
+    {
+      lastname: 'DE BOUARD',
+      firstname: 'Anne-claire'
+    },
+    {
+      lastname: 'DUMONET',
+      firstname: 'Vincent'
+    },
+    {
+      lastname: 'EL HACHIMI ALAOUI',
+      firstname: 'Imane'
+    },
+    {
+      lastname: 'GORMAND',
+      firstname: 'Loïc'
+    },
+    {
+      lastname: 'PROVO',
+      firstname: 'Sabine'
+    },
+    {
+      lastname: 'ROCK',
+      firstname: 'Bertrand'
+    },
+    {
+      lastname: 'ROFFIDAL',
+      firstname: 'Yann'
+    },
+    {
+      lastname: 'TROUCHE',
+      firstname: 'Boris'
+    },
+    {
+      lastname: 'TURTI',
+      firstname: 'Thomas'
     }
 
   ]
